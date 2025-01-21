39f1b10ac3eba15b5865edd4bb9c8bf4de26c3dd
Vicente Guzman <vicente.g.guzman@accenture.com>
2025-01-21 11:21:04 -0600

    Se agrega el controlador de Loading (Cargando)

--- RicknMortyAPI/src/app/tab2/tab2.page.ts
+++ RicknMortyAPI/src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -9,14 +10,23 @@ import { ApiService } from '../services/api.service';
 })
 export class Tab2Page {
 
-  public characters: any = [] 
-
-  constructor(private apiService: ApiService) {
+  public characters: any = [];
+  private loading: any;
+  constructor(private apiService: ApiService, private loadingController: LoadingController) {
     this.getCharacters()
   }
 
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      message: 'Cargando...',
+      duration: 2000
+    });
+    return await this.loading.present();
+}
+
   async getCharacters() {
     this.apiService.getCharacters().subscribe((res: any) => {
+      this.presentLoading()
       this.characters = res.results
     })
   }
