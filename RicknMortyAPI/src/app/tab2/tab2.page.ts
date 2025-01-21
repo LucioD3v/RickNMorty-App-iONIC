84808da07883c8fb17b9da2b4cef73aa8d3d58e8
Vicente Guzman <vicente.g.guzman@accenture.com>
2025-01-21 10:32:15 -0600

    Se muestra listado de la API de RicknMorty

--- RicknMortyAPI/src/app/tab2/tab2.page.ts
+++ RicknMortyAPI/src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-tab2',
@@ -8,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public characters: any = [] 
 
+  constructor(private apiService: ApiService) {
+    this.getCharacters()
+  }
+
+  async getCharacters() {
+    this.apiService.getCharacters().subscribe((res: any) => {
+      this.characters = res.results
+    })
+  }
 }
